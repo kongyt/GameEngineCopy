0471811e43defc5459593926c70b81128c445aa7
kongyatong <kongyatong@doodlemobile.com>
2018-05-15 17:52:31 +0800

    backup

---
+++ Framework/Common/GraphicsManager.hpp
@@ -0,0 +1,16 @@
+#ifndef __GraphicsManager_H__
+#define __GraphicsManager_H__
+
+#include "IRuntimeModule.hpp"
+
+namespace GE{
+    
+    class GraphicsManager : implements IRuntimeModule
+    {
+    public:
+        virtual ~GraphicsManager(){}        
+    };    
+    
+}
+
+#endif
